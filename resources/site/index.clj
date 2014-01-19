2f9f07c28815d31fee644182e8f9274324cc3db0
Isaac Boehman <isaac@bitsighttech.com>
2014-01-19 18:54:43 -0500

    using blacktie.co vcard theme

--- resources/site/index.clj
+++ resources/site/index.clj
@@ -1,38 +1,27 @@
-{:title "yacn.me"}
+{:title "Index"}
 
-[:div
- [:div.row
-  [:div.col-md-12
-   [:h1 "Yet Another Clever Name"]]]
- [:div.row
-  [:div.col-md-6
-   [:p.lead.text-muted
-    "Code and stuff."]]]
- [:div.row
-  [:div.col-md-12
-   [:p
-    "Welcome to yet another clever blog. This is where I'll post about things that interest me. "
-    "I'm passionate about building things, solving problems, and having fun. "
-    "My current tools of choice are Ruby, Python, and Bash. Recently, I've been dabling in Clojure as well to scratch my functional itch. "
-    "Here you'll find my " [:a {:href "/blog.html"} "blog"] ", and some of the " [:a {:href "/projects.html"} "projects"] " I'm working on, "
-    "and a little more " [:a {:href "/about.html"} "about"] " myself."]
-   [:p
-    "I'm active on " [:a {:href "https://github.com/yacn"} "GitHub"] " and " [:a {:href "https://twitter.com/yaclevername"} "tweet"] " occasionally."]]
- [:br]
- [:div.row
-  [:div.col-md-6
-   [:h4 "Recent Blog Posts"]
-   (map #(let [f % url (static.core/post-url f)
-               [metadata _] (static.io/read-doc f)
-               tags (.split (:tags metadata) " ")
-               date (static.core/parse-date
-                     "yyyy-MM-dd" "dd MMMM yyyy"
-                     (re-find #"\d*-\d*-\d*" (str f)))]
-    (if (not (some #{"is3500blog" "assignment1"} tags))
-     [:div
-      [:div [:a {:href url} (:title metadata)]
-      [:div date]]]))
-     (take 8 (reverse (static.io/list-files :posts))))]
-   [:div.col-md-6
-    [:h4 "Recent Activity on Github"]
-    [:div.gh-recent]]]]]
+[:div.col-md-8
+ [:div.tab-content
+  ; about tab
+  [:div {:class "tab-pane active" :id "about"}
+   [:h3 "Isaac Boehman"]
+   ;[:h5 "Title"]
+   [:hr]
+   ;[:p "Lorem Ipsum is simply dummy text of the printing and typsetting industry."]
+   [:p.sm 
+    "3rd-year undergrad at Northeastern University working towards a combined "
+    "Bachelors of Science in Computer and Information Science."]]
+  ; contact tab
+  [:div {:class "tab-pane" :id "contact"}
+   [:h4 "Contact Information"]
+   [:hr]
+   [:div.row
+    [:div.col-xs-6
+     [:p.sm
+      [:i.icon-linkedin.icon-2x] " - " [:a {:href "https://www.linkedin.com/in/idboehman/"} "idboehman "] [:br]
+      [:i.icon-globe.icon-2x] " - " [:a {:href "http://yacn.me"} "www.yacn.me "]]]
+    [:div.col-xs-6
+     [:p.sm
+      ;[:i.icon-envelope] " - isaac@isaacboehman.com " [:br]
+      [:i.icon-github.icon-2x] " - " [:a {:href "https://github.com/yacn"} "@yacn"] [:br]
+      [:i.icon-twitter.icon-2x] " - " [:a {:href "https://twitter.com/yaclevername"} "@yaclevername"]]]]]]]
